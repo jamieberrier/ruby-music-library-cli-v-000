c4f2aae347e30ece637d0731e3d93fb539165300
jamieberrier <berrier.jamie@gmail.com>
2019-04-03 17:01:11 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -177,7 +177,7 @@ class MusicLibraryController
     #upon receiving valid input 'plays' the matching song from the alphabetized list output by #list_songs
     @alphabetized_list.each do |song|
       if song.start_with?(input)
-        str = song.split(". ").drop(1)
+        output = song.split(". ").drop(1)
         output = str.split(" - ")
         name = output[1]
         artist = output[0]

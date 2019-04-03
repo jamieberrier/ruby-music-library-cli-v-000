d4abf3680a7928c4a23bfc8412f7e5af7cd6d2c0
jamieberrier <berrier.jamie@gmail.com>
2019-04-03 17:11:15 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -173,7 +173,7 @@ class MusicLibraryController
     #prompts the user to choose a song from the alphabetized list output by #list_songs
     puts "Which song number would you like to play?"
     input = gets.strip
-    
+
     #upon receiving valid input 'plays' the matching song from the alphabetized list output by #list_songs
     @alphabetized_list.each do |song|
       if song.start_with?(input)

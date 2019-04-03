00e547947ffa32ba6b2d320aa126fe795c020d86
jamieberrier <berrier.jamie@gmail.com>
2019-04-03 16:48:16 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -177,7 +177,7 @@ class MusicLibraryController
     #upon receiving valid input 'plays' the matching song from the alphabetized list output by #list_songs
     @alphabetized_list.each do |song|
       if song.start_with?(input)
-        song.split(" - ")
+        output = song.split(" - ")
         puts "Playing #{} by #{}"
       end
     end
